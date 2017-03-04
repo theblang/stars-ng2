40a824d0cebe22016ad413b5188db0fd5aaa7d33
Matt Langston <matt@mattblang.com>
2017-03-04 16:23:23 -0600

    Shorten the animate wrapper syntax

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -34,9 +34,7 @@ export class AppComponent {
     }
 
     animate() {
-        window.requestAnimationFrame(() => {
-            this.animate();
-        });
+        window.requestAnimationFrame(() => this.animate());
         this.mesh.rotation.x += 0.01;
         this.mesh.rotation.y += 0.02;
         this.renderer.render(this.scene, this.camera);
